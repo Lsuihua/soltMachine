3d684745242aeceb4066fadad9c7fa6b845b25b5
Lsuihua <37467467+Lsuihua@users.noreply.github.com>
2020-11-05 18:04:34 +0800

    小圆点

--- mechine.js
+++ mechine.js
@@ -65,10 +65,54 @@
       ctx.closePath();
       ctx.fillStyle = '#991401';
       ctx.fill();
+
+      // 画按钮
+      ctx.beginPath();
+      ctx.moveTo(-minW + 90,-4);
+      ctx.arcTo(minW -80,-4,minW-74,0,10);
+      ctx.lineTo(minW - 62,30);
+      ctx.arcTo(minW-60,52,minW-68,52,8);
+      ctx.arcTo(-minW+60,52,-minW+62,44,8);
+      ctx.lineTo(-minW + 62,30);
+      ctx.arcTo(-minW+80,-4,-minW+90,-4,10);
+      ctx.closePath();
+      let btBg = ctx.createLinearGradient(-minW + 90,0,minW - 90,0);
+      btBg.addColorStop(0,'#fe8502');
+      btBg.addColorStop(.5,'#fea70a');
+      btBg.addColorStop(1,'#fe8502');
+      ctx.fillStyle = btBg;
+      ctx.shadowBlur = 2;
+      ctx.shadowColor = '#a92306';
+      ctx.shadowOffsetY = 4;
+      ctx.fill();
+
+      ctx.beginPath();
+      ctx.moveTo(-minW + 90,-4);
+      ctx.arcTo(minW -80,-4,minW-74,0,10);
+      ctx.arcTo(minW - 60,34,minW - 65,34,5);
+      ctx.arcTo(-minW + 60,34,-minW + 62,28,5);
+      ctx.arcTo(-minW + 80,-4,-minW + 90,-4,10);
+      ctx.closePath();
+      let btBg2 = ctx.createLinearGradient(0,-4,0,34);
+      btBg2.addColorStop(0,'#ffed7d');
+      btBg2.addColorStop(1,'#ffe21a');
+      ctx.shadowBlur = 1;
+      ctx.shadowColor = '#fdf47b';
+      ctx.shadowOffsetY = 1;
+      ctx.fillStyle = btBg2;
+      ctx.fill();
+
+      ctx.save();
+      ctx.font = '24px STheiti, SimHei';
+      ctx.fillStyle = '#fc3132';
+      ctx.textAlign = 'center';
+      ctx.textBaseline = 'middle';
+      ctx.fillText('点击抽奖',0,15);
+      ctx.restore();
     }
     // 顶部
     function drawTopBG(){
-      var th = Game.bkCanvas.width/2 - 20.
+      var th = Game.bkCanvas.width/2 - 20;
           tw = Game.bkCanvas.width/2 - 38;
       ctx.save();
       ctx.beginPath();
@@ -165,13 +209,14 @@
     }
 
     function drawContent(tw,th){
+      var br= 20,sr = 10;
       ctx.save();
       ctx.beginPath();
-      ctx.moveTo(-tw+44,-th+20);
-      ctx.arcTo(tw-24,-th+20,tw-24,-th+40,20);
-      ctx.arcTo(tw-24,-20,tw-44,-20,20);
-      ctx.arcTo(-tw+24,-20,-tw+24,-40,20);
-      ctx.arcTo(-tw+24,-th+20,-tw+44,-th+20,20);
+      ctx.moveTo(-tw+44,-th+14);
+      ctx.arcTo(tw-24,-th+14,tw-24,-th+14 + br,br);
+      ctx.arcTo(tw-24,-20,tw-24 -br,-20,br);
+      ctx.arcTo(-tw+24,-20,-tw+24,-20 -br,br);
+      ctx.arcTo(-tw+24,-th+14,-tw+24+br,-th+14,br);
       ctx.closePath();
       ctx.save();
       ctx.fillStyle = '#feca05';
@@ -181,28 +226,95 @@
       ctx.restore();
 
       ctx.beginPath();
-      ctx.moveTo(-tw+54,-th+34);
-      ctx.arcTo(tw-38,-th+34,tw-38,-th+44,20);
-      ctx.arcTo(tw-38,-34,tw-58,-34,20);
-      ctx.arcTo(-tw+38,-34,-tw+38,-54,20);
-      ctx.arcTo(-tw+38,-th+34,-tw+54,-th+34,20);
+      ctx.moveTo(-tw+54,-th+30);
+      ctx.arcTo(tw-38,-th+30,tw-38,-th+30 +sr,sr);
+      ctx.arcTo(tw-38,-34,tw-38 -sr,-34,sr);
+      ctx.arcTo(-tw+38,-34,-tw+38,-34-sr,sr);
+      ctx.arcTo(-tw+38,-th+30,-tw+30+sr,-th+30,sr);
       ctx.closePath();
       ctx.fillStyle = '#eb6f0d';
       ctx.fill();
 
       ctx.beginPath();
-      ctx.moveTo(-tw+54,-th+38);
-      ctx.arcTo(tw-38,-th+38,tw-38,-th+56,16);
-      ctx.arcTo(tw-38,-34,tw-58,-34,20);
-      ctx.arcTo(-tw+38,-34,-tw+38,-54,20);
-      ctx.arcTo(-tw+38,-th+38,-tw+54,-th+38,16);
+      ctx.moveTo(-tw+54,-th+34);
+      ctx.arcTo(tw-38,-th+34,tw-38,-th+34+sr,sr);
+      ctx.arcTo(tw-38,-34,tw-38-sr,-34,sr);
+      ctx.arcTo(-tw+38,-34,-tw+38,-34-sr,sr);
+      ctx.arcTo(-tw+38,-th+34,-tw+38+sr,-th+34,sr);
       ctx.closePath();
       ctx.fillStyle = '#c70c05';
       ctx.fill();
+      ctx.clip();
+      ctx.restore();
 
+      drawPoint(-tw+24,-th+20,tw-24,-20);
+
+      prizeContent(-tw+38,-th+34,tw-38,-34);
+    }
+
+    
+    // 画奖品内容
+    function prizeContent(sw,sh,ew,eh){
+      ctx.save();
+      const num = 3,
+            pd = 8;
+      let _w = ew - sw,
+          rw = (_w - pd * (num + 1)) /num,
+          rh = eh - sh;
+      var prizeBg = ctx.createLinearGradient(sw,sh,sw,eh);
+      prizeBg.addColorStop(0,'#bcbcbc');
+      prizeBg.addColorStop(.3,'#f4f4f6');
+      prizeBg.addColorStop(.5,'#ffffff');
+      prizeBg.addColorStop(.7,'#f4f4f6');
+      prizeBg.addColorStop(1,'#bcbcbc');
+      ctx.fillStyle = prizeBg;
+      for(let i = 1; i<=num;i++){  
+        ctx.beginPath();
+        ctx.fillRect(sw + i*pd + (i-1)*rw,sh,rw,rh);
+        ctx.closePath();
+      }
       ctx.restore();
     }
 
+    function drawPoint(sx,sy,ex,ey){
+      ctx.save();
+      let num = 20,
+          r = 4;
+      var l = ((ey - sy) + (ex - sx))*2;
+      // console.log(l)
+      // console.log((ex - sx))
+      // console.log((ex - sx)+(ey-sy))
+      // 计算间距
+      var mg = l/num,
+          pd = 2,
+          x = 0,
+          y = 0,
+          dx = 0,
+          dy = 0;
+      for(let i = 0;i<num;i++){
+        dx = 2*r + i*mg + pd;
+        if(dx<ex - sx){
+          x = sx + dx;
+          y = sy + pd;
+        }else if(dx>ex - sx && dx < l/2){
+          dy += mg
+          x = ex - 2*r + pd/2;
+          y = sy + dy;
+        }else if(dx > l/2 && dx < l - (ey-sy)){
+          x = sx;
+          y = ey - pd;
+        }else{
+          x = sx + 2*r - pd/2;
+          y = sy + pd + mg;
+        }
+        ctx.beginPath();
+        ctx.arc(x,y,r,0,Math.PI*2);
+        ctx.closePath();
+        ctx.fillStyle = '#fff';
+        ctx.fill();
+      }
+      ctx.restore();
+    }
     ctx.restore();
   }
 
